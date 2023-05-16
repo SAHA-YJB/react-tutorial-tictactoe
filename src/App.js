7fe42bb9ac0cbfdcb8094651e55d7b80547c6246
SAHA-YJB <pkomom123@naver.com>
2023-05-16 22:45:10 +0900

    time travel

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
+import { useState } from 'react';
 import './App.css';
 import Board from './components/Board';
 
 function App() {
+
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calcWinner = (squares) => {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+    for (let i = 0; i < lines.length; i++) {
+      const [a,b,c] = lines[i]
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a]
+      }       
+    }
+    return null
+  }
+
+  const current = history[history.length - 1]
+  const winner = calcWinner(current.squares)
+
+  let status;
+  if (winner) {
+    status = 'Winner:'+ winner
+  } else {
+    status = `Next Player ${xIsNext ? 'X' : 'O'}`
+  }
+
+  const handleClick = (i) => {
+    const newSquares = current.squares.slice()
+    if (calcWinner(newSquares) || newSquares[i]) {
+      return;
+   }
+    newSquares[i] = xIsNext ? 'X' : 'O'  
+    setHistory([...history, { squares: newSquares }]) 
+    setXIsNext(current => !current)
+  }
+
   return (
     <div className="game">
+      
       <div className="game-board">
-        <Board/>
+        <Board squares={current.squares} onClick={(i) => handleClick(i)}/>
       </div>
 
       <div className="game-info">
-      game-i
+        <div className='status'>{status}</div>
       </div>
       
     </div>
